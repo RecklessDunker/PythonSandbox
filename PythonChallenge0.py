ba4ea04ab8958cc1499b03f30e989740cf5d177b
jim <admin@jimgriff.co.uk>
2019-06-29 17:48:12 +0100

    Python Challenge 0 - see file header for details

---
+++ PythonChallenge0.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------
+# Author: James Griffiths
+# Date Created: Wednesday, 26th June 2019
+# Version: 1.0
+# --------------------------------------------------------
+
+
+# Challenge One ------------------------------------------
+# Description: http://www.pythonchallenge.com/pc/def/0.html
+# I think the challenge wants me to work out 2**38...
+# Which the below is 274877906944 add the suffix: 274877906944.html
+
+# My solution:
+print(2**38)
